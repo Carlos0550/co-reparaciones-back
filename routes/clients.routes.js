3d2eaf4837a031ebd34416bb371a0e1ea3c6de85
carlos0550 <carlospelinski03@gmail.com>
2024-12-23 02:03:09 -0300

    Nuevas rutas

--- routes/clients.routes.js
+++ routes/clients.routes.js
@@ -1,10 +1,15 @@
 const express = require("express")
-const { createClient, verifyAuthCode, loginClientWithEmail } = require("../controllers/clients.controller")
+const { createClient, verifyAuthCode, loginClientWithEmail, saveClientInfo, getClientInfo } = require("../controllers/clients.controller")
 const Router = express.Router()
+const multer = require("multer")
 
+const storage = multer.memoryStorage()
+const upload = multer({storage})
 
 Router.post("/new-client/:userEmail", createClient)
 Router.put("/verify-auth-code", verifyAuthCode)
 Router.put("/login-client/:email", loginClientWithEmail)
+Router.post("/save-client-info", upload.none(), saveClientInfo)
+Router.get("/retrieve-client-info", getClientInfo)
 
 module.exports = Router
