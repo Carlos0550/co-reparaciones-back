b5e13d13c39ab16016339f309d378da24e2e5d89
carlos0550 <carlospelinski03@gmail.com>
2024-12-31 15:58:34 -0300

    ajustes menores

--- Mercado_pago/MercadoPago.controller.js
+++ Mercado_pago/MercadoPago.controller.js
@@ -7,11 +7,13 @@ const mercadoPago = new MercadoPagoConfig({
 
 
 const createPayment = async (req, res) => {
-    const { products } = req.body
-
-    if(!products) return res.status(400).json({ msg: "El servidor no recibió correctamente la información del producto" })
+    const { products, client_data } = req.body
     
+    if(!products || !client_data) return res.status(400).json({ msg: "El servidor no recibió correctamente la información del producto" })
+    
+
     try {
+        const parsedClientData = JSON.parse(client_data)
         const preference = new Preference(mercadoPago)
         const parsedProducts = JSON.parse(products).map(element => {
             return {
@@ -20,19 +22,35 @@ const createPayment = async (req, res) => {
                 unit_price: Number(element.unit_price),
                 quantity: Number(element.quantity)
             }})
-    
+    //https://co-reparaciones-front.vercel.app/
         const response = await preference.create({
             body: {
                 items: parsedProducts,
                 back_urls: {
-                    success: "https://co-reparaciones-front.vercel.app/payment-success", 
-                    failure: "https://co-reparaciones-front.vercel.app/payment-failure",  
-                    pending: "https://co-reparaciones-front.vercel.app/payment-pending"  
+                    success: "http://localhost:5173/payment-success", 
+                    failure: "http://localhost:5173/payment-failure",  
+                    pending: "http://localhost:5173/payment-pending"  
                 },
                 auto_return: "approved",
+                payer: {
+                    name: parsedClientData[0].user_fullname,
+                    email: parsedClientData[0].user_email,
+                    phone: {
+                      area_code: "+54",
+                      number: parsedClientData[0].user_phone || "1234567890",
+                    },
+                    identification: {
+                      type: 'DNI',
+                      number: parsedClientData[0].dni,
+                    },
+                    address: {
+                      zip_code: parsedClientData[0].postal_code || "",
+                      street_name: parsedClientData[0].first_address || "",
+                    },
+                  },
               }
         });
-        
+        console.log(response)
         if(response){ 
             return res.status(200).json({
             init_point: response.init_point,
